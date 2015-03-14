9d7da1f59c4ffde7363d18bda5a8b26f7e861db6
tobe <tobeg3oogle@gmail.com>
2015-03-14 20:12:33 +0800

    Add bower swipe

---
+++ js/style.js
@@ -0,0 +1,11 @@
+
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+  startSlide: 2,
+  speed: 400,
+  auto: 3000,
+  continuous: true,
+  disableScroll: false,
+  stopPropagation: true,
+  callback: function(index, elem) {},
+  transitionEnd: function(index, elem) {}
+});
